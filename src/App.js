af6c7e3ed988a028908114e0e245b0d001a340c6
Sujeong Youn <sujungyoun92@gmail.com>
2022-04-25 17:23:35 +0900

    [+] Sidebar 이름 및 아이콘 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css'
+import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar.jsx";
 
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeepscode
+      <div className="AppGlass">
+        <Sidebar />
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
