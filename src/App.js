349a153fbc55f37c9e2728cde8df42a0f4a10571
Sujeong Youn <sujungyoun92@gmail.com>
2022-04-26 17:30:02 +0900

    [+] Main Dashboard 화면

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar.jsx";
+import MainDash from "./MainDash/MainDash";
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
         <Sidebar />
-        <div></div>
+        <MainDash />
         <div></div>
       </div>
     </div>
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../imgs/logo.png'
 import './Sidebar.css'
 
@@ -6,6 +6,9 @@ import { SidebarData } from "../../Data/Data";
 import { UilSignOutAlt } from "@iconscout/react-unicons";
 
 const Sidebar = () => {
+
+    const [selected, setSelected] = useState(0);
+
   return (
     <div className="Sidebar">
         {/* logo */}
@@ -20,7 +23,10 @@ const Sidebar = () => {
         <div className="menu">
            {SidebarData.map((item, index) => {
                return(
-                   <div className="menuItem">
+                   <div className={selected===index?'menuItem active': 'menuItem'}
+                   key={index}
+                   onClick={()=>setSelected(index)}
+                   >
                        <item.icon/>
                        <span>
                            {item.heading}
